b4e4d7d5c8147c1954e7acc7aa8902c8e3adae63
mangjaki <cungcang996@gmail.com>
2024-06-22 02:43:51 +0700

    update

--- SekolahApp/routes/web.php
+++ SekolahApp/routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\JurusanController;
+use App\Http\Controllers\KelasController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('layout.main');
 });
+
+Route::resource('jurusan', JurusanController::class);
+Route::resource('kelas', KelasController::class);
